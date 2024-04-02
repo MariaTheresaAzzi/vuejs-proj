92ae56c2fea3c80db05f1b900e99fc8ad3f526e0
MariaTheresaAzzi <mariatheresaelkazzi@gmail.com>
2024-04-02 17:17:25 +0300

    added routing for app and the Ui for the pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="flex-row">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </div>
   </div>
 </template>
+
+<style>
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="contact">
+    <h1>Contact Us</h1>
+    <p>Feel free to contact us for any inquiries or feedback.</p>
+    <form class="flex">
+      <label>Full Name</label>
+      <input type="text" v-model="fullName" />
+
+      <label>Email</label>
+      <input type="email" v-model="email" />
+
+      <label>Message</label>
+      <textarea type="text" v-model="message"></textarea>
+
+      <button :disabled="isButtonDisabled">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fullName: '',
+      email: '',
+      message: ''
+    };
+  },
+  computed: {
+    isButtonDisabled() {
+      return !this.fullName || !this.email || !this.message;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input, textarea {
+  margin-bottom: 15px;
+  width: 100%;
+  padding: 4px;
+  border-radius: 12px;
+}
+
+p {
+  margin-bottom: 35px;
+}
+
+textarea {
+  height: 100px;
+}
+
+button {
+  background-color: #42b983;
+  padding: 5px;
+  border: 0px;
+  border-radius: 12px;
+  font-weight: 700;
+}
+</style>
